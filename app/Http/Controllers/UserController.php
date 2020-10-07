972eb115392e6d70dc4a92d34d8389db9e1cfb0b
Robbin Ploeger <robbplo@gmail.com>
2020-10-07 19:33:52 +0200

    Reproduce phpstan error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,25 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
 use App\User;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/user",
+     *     @OA\Response(
+     *         response="200",
+     *         description="Return user",
+     *         @OA\JsonContent(ref="#/components/schemas/User"),
+     *     )
+     * )
+     *
+     * @return UserResource
+     */
     public function index()
     {
-        /**
-         * @OA\Get(
-         *     path="/user",
-         *     @OA\Response(
-         *         response="200",
-         *         description="Return user",
-         *         @OA\JsonContent(ref="#/components/schemas/User"),
-         *     )
-         * )
-         *
-         * @return UserResource
-         */
-
         $user = factory(User::class)->create();
 
         return UserResource::make($user);
